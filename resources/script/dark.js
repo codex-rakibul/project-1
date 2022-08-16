ecf061e25efd00ac5076f60dd921ac907bcbc219
rakibul667s <rakibulislam667s@gmail.com>
2022-08-16 11:00:13 +0600

    file added

--- resources/script/dark.js
+++ resources/script/dark.js
@@ -3,11 +3,14 @@ const dark = document.querySelector('#dark');
 
 function darkmode(){
   const header2 = document.querySelector('.header2');
+  const dark= document.querySelector('.dark');
   header2.style.background="black";
-  onclick="normal()";
+  dark.style.color="black";
+  return true;
   
 }
 function lightmode(){
   const header = document.querySelector('.header2');
   header.style.background="white";
+  return true;
 }
